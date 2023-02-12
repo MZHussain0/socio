65616f0bf8cf8031c441d698effcf1d8cde0283a
MZHussain <MZHussain092@gmail.com>
2023-02-12 20:07:13 +0530

    implemented routes auth, user, posts

--- server/server.js
+++ server/server.js
@@ -8,6 +8,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS //
 const __filename = fileURLToPath(import.meta.url);
@@ -37,6 +46,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES //
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+// ROUTES //
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP //
 const PORT = process.env.PORT || 3500;
@@ -46,6 +61,8 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
+    // User.insertMany(users);
+    // Post.insertMany(posts);
     app.listen(PORT, () => console.log(`server is running at ${PORT}`));
   })
   .catch((error) => console.log(`${error} "did not connect"`));
